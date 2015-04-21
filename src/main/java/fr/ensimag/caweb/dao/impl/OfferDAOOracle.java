7e9e385ea6d63330578dca2e2dbdbf06924bceee
Alexandre Rupp <alexandre.rupp@ensimag.grenoble-inp.fr>
2015-04-21 10:49:59 +0200

    avancées sur Offer

--- src/main/java/fr/ensimag/caweb/dao/impl/OfferDAOOracle.java
+++ src/main/java/fr/ensimag/caweb/dao/impl/OfferDAOOracle.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package fr.ensimag.caweb.dao.impl;
 
 import fr.ensimag.caweb.dao.DAOException;
@@ -12,6 +12,7 @@ import fr.ensimag.caweb.models.Contract.Contract;
 import fr.ensimag.caweb.models.Offer;
 import fr.ensimag.caweb.models.Quantity;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,44 +29,81 @@ import javax.sql.DataSource;
  */
 
 public class OfferDAOOracle implements OfferDAO {
-
+    
     private final DAOFactory daoFactory;
-
+    
     private static final String selectAll
             = "SELECT * FROM Offre";
-
+    
     private static final String selectQuantities
-            = "SELECT * FROM Quantite WHERE idOffrePrecisee = ?";  
-            
+            = "SELECT * FROM Quantite WHERE idOffrePrecisee = ?";
+    
+    private static final String insertQuery
+            = "INSERT INTO Contrat (offreur, demandeur, dateContrat, nomProduitContrat, "
+            + " prixLotContrat, dureeContrat, qteLotContrat, uniteContrat, nbLots, aRenouveler) "
+            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, 0)";
+    
+    
+    
     public OfferDAOOracle(DAOFactory daoFactory) {
-           this.daoFactory = daoFactory;
+        this.daoFactory = daoFactory;
     }
-
+    
     @Override
-    public Offer create(Offer obj) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Contract create(Contract contrat) throws DAOException {
+        Connection connec = daoFactory.getConnection();
+        PreparedStatement insertPrep = null;
+        try {
+            insertPrep = connec.prepareStatement(insertQuery);
+            insertPrep.setString(1, contrat.getOffreur().getPseudo());
+            insertPrep.setString(2, contrat.getDemandeur().getPseudo());
+            insertPrep.setDate(3, (Date)contrat.getDateContrat());
+            insertPrep.setString(4, contrat.getNomProduitContrat());
+            insertPrep.setDouble(5, contrat.getPrixTotal());
+            insertPrep.setInt(6, contrat.getDuree());
+            insertPrep.setInt(7, contrat.getNbLots());
+            insertPrep.setString(8, contrat.getQuantite().getUniteQte());
+            insertPrep.setInt(9, contrat.getNbLots());
+            insertPrep.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ContractDAOOracle.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            throw new DAOException("Erreur BD " + ex.getMessage(), ex);
+        } finally {
+            try {
+                if (insertPrep != null) {
+                    insertPrep.close();
+                }
+                daoFactory.closeConnection(connec);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                throw new DAOException("Erreur BD " + ex.getMessage(), ex);
+            } finally {
+                return contrat;
+            }
+        }
     }
-
+    
     @Override
     public Offer read(int id) throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public List<Offer> readAll() throws DAOException {
         List<Offer> result = new ArrayList<Offer>();
-        ResultSet rs = null;       
+        ResultSet rs = null;
         Connection conn = null;
         PreparedStatement selectPrep = null;
         try {
             conn = daoFactory.getConnection();
-            Statement st = conn.createStatement();           
+            Statement st = conn.createStatement();
             rs = st.executeQuery(selectAll);
             while (rs.next()) {
                 //Get the quantities
                 List<Quantity> quantities = new ArrayList<Quantity>();
                 selectPrep = conn.prepareStatement(selectQuantities);
-                selectPrep.setInt(1, rs.getInt("idOffre")); 
+                selectPrep.setInt(1, rs.getInt("idOffre"));
                 ResultSet rs_qte = selectPrep.executeQuery();
                 while (rs_qte.next()) {
                     int qte = rs_qte.getInt("qte");
@@ -82,15 +120,15 @@ public class OfferDAOOracle implements OfferDAO {
         } finally {
             daoFactory.closeConnection(conn);
         }
-        return result;        
+        return result;
     }
-
-
+    
+    
     @Override
     public Offer update(Offer obj) throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public void delete(Offer obj) throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
