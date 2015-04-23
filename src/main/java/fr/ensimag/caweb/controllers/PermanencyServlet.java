dc28bd42b9e7529e4d91b637d695ea2491347247
Alexandre Rupp <alexandre.rupp@ensimag.grenoble-inp.fr>
2015-04-23 19:16:54 +0200

    suppression d'un include obsolete et non utilisé

--- src/main/java/fr/ensimag/caweb/controllers/PermanencyServlet.java
+++ src/main/java/fr/ensimag/caweb/controllers/PermanencyServlet.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.json.Json;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -33,7 +32,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import sun.misc.IOUtils;
 
 /**
  *
